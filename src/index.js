16723a0579feccc8867604507835240f2f327a14
bot5294 <5294aqib@gmail.com>
2022-09-24 03:54:47 +0530

    redux store is persistant now

--- src/index.js
+++ src/index.js
@@ -6,9 +6,15 @@ import { Provider } from "react-redux";
 // import reducers from "./reducers";
 import store from "./store";
 // let store = configureStore();
+import { persistStore } from "redux-persist";
+import { PersistGate } from "redux-persist/integration/react";
+//...
+let persistor = persistStore(store);
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <Provider store={store}>
-    <App />
+    <PersistGate persistor={persistor}>
+      <App />
+    </PersistGate>
   </Provider>
 );
--- src/store/index.js
+++ src/store/index.js
@@ -1,12 +1,36 @@
-import { createStore } from "redux";
-
+import { configureStore } from "@reduxjs/toolkit";
+import storage from "redux-persist/lib/storage";
+import {
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from "redux-persist";
 import reducer from "../reducers/index.js";
 
-let store;
-function configureStore(state = {}) {
-  store = createStore(reducer, state);
+const persistConfig = {
+  key: "counter",
+  storage,
+};
+const persistedReducer = persistReducer(persistConfig, reducer);
+export default configureStore({
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+      immutableCheck: false,
+    }),
+});
+// let store;
+// function configureStore(state = {}) {
+//   store = createStore(reducer, state);
 
-  return store;
-}
+//   return store;
+// }
 
-export default configureStore();
+// export default configureStore();
